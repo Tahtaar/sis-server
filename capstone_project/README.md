67203084bf51b8126c775bc2ae05ffcd8ca61a55
Tahta arya <91930096+Tahtaar@users.noreply.github.com>
2024-07-19 20:58:41 +0700

    Delete capstone_project directory

--- capstone_project/README.md
+++
@@ -1,8 +0,0 @@
-# Capstone Project
-
-## Setup and Run the Application
-
-1. Clone the repository:
-   ```sh
-   git clone <repository_url>
-   cd capstone_project
--- capstone_project/schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE User (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(80) NOT NULL,
-    password_hash VARCHAR(128) NOT NULL,
-    UNIQUE(username)
-);
-
--- Additional table definitions go here
